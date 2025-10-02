6746f83a87a90a0b194335f8a8b033c2456e1953
Trinh Nhat Quang <trinhnhatquang99@gmail.com>
2025-10-02 18:21:24 +0700

    lien ket

---
+++ src/demo2_github/Demo2_github.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package demo2_github;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Demo2_github {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("demo2_github ohhhh");
+    }
+    
+}
